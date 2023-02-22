0299831fa1adaa4fe7c09f2118e8bf15766c928f
Alpha Master <20980083+AlphaPlusProgrammers@users.noreply.github.com>
2023-02-21 17:06:58 -0800

    changed motor to a spark max on CAN ID 4

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,6 @@ import frc.robot.commands.MoveMotorCom;
 import frc.robot.subsystems.MoveMotorSub;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public final MoveMotorSub moveMotorSub = new MoveMotorSub();
